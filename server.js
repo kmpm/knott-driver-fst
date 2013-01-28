c2a1073a95c355dbfebc430d5cd9069ac7f8da6d
kmpm <peter@birchroad.net>
2013-01-28 09:38:44 +0100

    Added logging

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var mqtt = require('mqttjs'),
     easyip = require('easyip'),
-    os = require('os');
+    os = require('os'),
+    Log = require('./lib/log');
 
 
 //load config file
-var config = require('./config.json');
+var config = require('./config.json'),
+    log = new Log();
+
+log.level = config.loglevel || 3;
 
 var ctopic = "/config/fst/deviceinfo/" + config.device_id + "/";
 
@@ -12,7 +16,7 @@ var subscribe, unsubscribe;
 
 var service = new easyip.Service();
 service.on('listening', function(){
-  console.log("easyip is online");
+  log.info("easyip is online");
   getConfig();
 });
 
@@ -24,17 +28,17 @@ service.bind();
 function getConfig(){
   mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
     if(err){
-      console.log("mqtt client creation error", err);
+      log.error("mqtt config creation error", err);
       process.exit(1);
     }
     client.connect({keepalive:config.mqtt.keepalive || 30000});
 
     client.on('error', function(err){
-      console.log("mqtt error", err);
+      log.error("mqtt error", err);
     });
 
     client.on('connack', function(){
-      console.log("connected to mqtt", config.mqtt);
+      log.info("Connected to mqtt config", config.mqtt);
       var topic = ctopic + '#';
       client.subscribe({topic:topic});
       client.publish({topic:ctopic + "booted" ,retain:true, payload:(new Date()).toString()});
@@ -42,10 +46,10 @@ function getConfig(){
 
     client.on('publish', function(packet){
       var topics = packet.topic.replace(ctopic, '').split('/');
-      console.log("topics", topics);
+      //log.debug("topics", topics);
       var key = topics[0];
       config[topics[0]]=packet.payload;
-      console.log(key.substring(0,2));
+      //log.debug(key.substring(0,2));
       if(key.substring(0,2) === 'fw'){
         if(packet.payload.length > 0){
           subscribe(key, packet.payload);
@@ -54,7 +58,6 @@ function getConfig(){
          unsubscribe(key); 
         }
       }
-
     });
   });
 }
@@ -71,22 +74,22 @@ function hartbeat(mqclient){
 mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
   var index = {};
   if(err){
-    console.log("mqtt client creation error", err);
+    log.debug("mqtt client creation error", err);
     process.exit(1);
   }
   client.connect({keepalive:config.mqtt.keepalive || 30000});
 
 	client.on('connack', function(packet){
-		console.log("Connected to mqtt");
+		log.debug("Connected to mqtt data", config.mqtt);
     client.publish({topic:'/config/fst/nodes/' + config.device_id +  "/hostname", payload:os.hostname(), retain:true});  
     hartbeat(client);
     setInterval(hartbeat, 30000, client);
 	});
   client.on('error', function(err){
-    console.log("mqtt error", err);
+    log.debug("mqtt error", err);
   });
   subscribe = function(address, topic){
-    console.log("listening", address, topic);
+    log.info(address, 'getting values from', topic); 
     index[topic]=address;
     client.subscribe({topic:topic});
   };
@@ -96,6 +99,7 @@ mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
     var toRemove;
     for(topic in index){
       if(index[topic] === address){
+        log.info(address, "no longer getting values from", topic);
         client.unsubscribe('topic');
         toRemove=topic;
         continue;
@@ -111,12 +115,12 @@ mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
     if(index.hasOwnProperty(packet.topic)){
       var address = parseInt(index[packet.topic].substring(2), 10);
       var value = Math.round(parseFloat(packet.payload)*100);
-      console.log("setting fw", address, "to", value);
+      //log.debug("setting fw", address, "to", value);
       service.storage.set(easyip.OPERANDS.FLAGWORD, address , value);
     }
   });
   service.storage.on('changed', function(operand, index, prev, now){
-    console.log("storage changed", operand, index, now);
+    log.debug("fw", index, "changed to", now);
     client.publish({topic:'/raw/fst/' + config.device_id + '/fw' + index, retain:true, payload:now});
   });
 });
